2dce289b5d14fce27af38aa6f1375980dede23db
Paul Battley <pbattley@gmail.com>
2019-10-20 22:45:46 +0100

    Report errors with location

--- parse.js
+++ parse.js
@@ -6,5 +6,9 @@ const filename = process.argv[2];
 console.log(`Processing ${filename}`);
 var input = fs.readFileSync(filename, 'UTF-8');
 
-var tree = parser.parse(input);
-console.log("Parsed: "+ util.inspect(tree, {depth: 10}));
+try {
+  var tree = parser.parse(input);
+  console.log(`Parsed: ${util.inspect(tree, {depth: 10})}`);
+} catch(e) {
+  console.log(`Error at line ${e.location.start.line}, column ${e.location.start.column}: ${e.message}`);
+}

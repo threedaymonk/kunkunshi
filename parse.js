a633470190e3b3a849ed2718225f9da89cb91177
Paul Battley <pbattley@gmail.com>
2019-10-23 17:03:12 +0100

    Group notes into cells

--- parse.js
+++ parse.js
@@ -4,11 +4,25 @@ const parser = require("./aor-parser.js");
 
 const filename = process.argv[2];
 console.log(`Processing ${filename}`);
-var input = fs.readFileSync(filename, 'UTF-8');
+let input = fs.readFileSync(filename, 'UTF-8');
+let tree;
 
 try {
-  var tree = parser.parse(input);
+  tree = parser.parse(input);
   console.log(`Parsed: ${util.inspect(tree, {depth: 10})}`);
 } catch(e) {
   console.log(`Error at line ${e.location.start.line}, column ${e.location.start.column}: ${e.message}`);
 }
+
+let sequence = [];
+let offset = 0;
+
+tree.music.forEach(note => {
+  let cell = Math.floor(offset / 2);
+  let subOffset = offset % 2;
+  sequence[cell] = sequence[cell] || [];
+  sequence[cell][subOffset] = note;
+  offset = offset + note.length * 2;
+});
+
+console.log(util.inspect(sequence));

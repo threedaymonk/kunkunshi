1d821dd99322bd6876a6f487fefdffd4eaaaf85d
Paul Battley <pbattley@gmail.com>
2019-11-30 20:22:58 +0000

    Reformat expectations consistently

--- test/parser-test.js
+++ test/parser-test.js
@@ -37,12 +37,11 @@ describe("Parser", function() {
         a o r
       `;
 
-      expect(parser.parse(input).music)
-        .to.eql([
-          {type: "note", position: "a", duration: 1},
-          {type: "note", position: "o", duration: 1},
-          {type: "note", position: "r", duration: 1},
-        ]);
+      expect(parser.parse(input).music).to.eql([
+        {type: "note", position: "a", duration: 1},
+        {type: "note", position: "o", duration: 1},
+        {type: "note", position: "r", duration: 1},
+      ]);
     });
 
     it("parses basic positions", function() {
@@ -54,12 +53,11 @@ describe("Parser", function() {
         k 5 6 7 8 9
       `;
 
-      expect(parser.parse(input).music.map(n => n.position))
-        .to.eql([
-          "a", "o", "r",
-          "4", "z", "t", "s",
-          "k", "5", "6", "7", "8", "9"
-        ]);
+      expect(parser.parse(input).music.map(n => n.position)).to.eql([
+        "a", "o", "r",
+        "4", "z", "t", "s",
+        "k", "5", "6", "7", "8", "9"
+      ]);
     });
 
     it("parses 下-prefixed positions (g = ge)", function() {
@@ -69,8 +67,9 @@ describe("Parser", function() {
         gs gr
       `;
 
-      expect(parser.parse(input).music.map(n => n.position))
-        .to.eql(["gs", "gr"]);
+      expect(parser.parse(input).music.map(n => n.position)).to.eql([
+        "gs", "gr"
+      ]);
     });
 
     it("parses 亻-prefixed positions (n = ninben)", function() {
@@ -82,12 +81,11 @@ describe("Parser", function() {
         ns nk n5 n6 n7
       `;
 
-      expect(parser.parse(input).music.map(n => n.position))
-        .to.eql([
-          "na", "no",
-          "nr", "n4", "nz", "nt",
-          "ns", "nk", "n5", "n6", "n7"
-        ]);
+      expect(parser.parse(input).music.map(n => n.position)).to.eql([
+        "na", "no",
+        "nr", "n4", "nz", "nt",
+        "ns", "nk", "n5", "n6", "n7"
+      ]);
     });
 
     it("parses 下- and 亻-prefixed positions", function() {
@@ -97,8 +95,9 @@ describe("Parser", function() {
         gns gnr
       `;
 
-      expect(parser.parse(input).music.map(n => n.position))
-        .to.eql(["gns", "gnr"]);
+      expect(parser.parse(input).music.map(n => n.position)).to.eql([
+        "gns", "gnr"
+      ]);
     });
 
     it("parses note durations", function() {
@@ -108,8 +107,9 @@ describe("Parser", function() {
         a a/ a/2 a1/2 a/4 a3/2 a2
       `;
 
-      expect(parser.parse(input).music.map(n => n.duration))
-        .to.eql([1, 0.5, 0.5, 0.5, 0.25, 1.5, 2]);
+      expect(parser.parse(input).music.map(n => n.duration)).to.eql([
+        1, 0.5, 0.5, 0.5, 0.25, 1.5, 2
+      ]);
     });
 
     it("parses rests", function() {
@@ -119,12 +119,11 @@ describe("Parser", function() {
         0 0/2 03/4
       `;
 
-      expect(parser.parse(input).music)
-        .to.eql([
-          {type: "rest", duration: 1},
-          {type: "rest", duration: 0.5},
-          {type: "rest", duration: 0.75}
-        ]);
+      expect(parser.parse(input).music).to.eql([
+        {type: "rest", duration: 1},
+        {type: "rest", duration: 0.5},
+        {type: "rest", duration: 0.75}
+      ]);
     });
 
     it("applies repetition start marker to next note", function() {
@@ -134,8 +133,9 @@ describe("Parser", function() {
         a -> a A> a B> a C> a a
       `;
 
-      expect(parser.parse(input).music.map(n => n.mark))
-        .to.eql([undefined, "A", "A", "B", "C", undefined]);
+      expect(parser.parse(input).music.map(n => n.mark)).to.eql([
+        undefined, "A", "A", "B", "C", undefined
+      ]);
     });
 
     it("applies repetition end marker to preceding note", function() {
@@ -145,8 +145,9 @@ describe("Parser", function() {
         a a <- a <A a <B a <C a
       `;
 
-      expect(parser.parse(input).music.map(n => n.jump))
-        .to.eql([undefined, "A", "A", "B", "C", undefined]);
+      expect(parser.parse(input).music.map(n => n.jump)).to.eql([
+        undefined, "A", "A", "B", "C", undefined
+      ]);
     });
 
     it("applies articulations to notes", function() {
@@ -156,8 +157,9 @@ describe("Parser", function() {
         a a' a1/2' a^ a/^
       `;
 
-      expect(parser.parse(input).music.map(n => n.articulation))
-        .to.eql([undefined, "hammer", "hammer", "upstroke", "upstroke"]);
+      expect(parser.parse(input).music.map(n => n.articulation)).to.eql([
+        undefined, "hammer", "hammer", "upstroke", "upstroke"
+      ]);
     });
 
     it("combines notes in chords", function() {
